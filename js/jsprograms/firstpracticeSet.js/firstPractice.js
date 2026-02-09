de7e7a4a5bc28e2d321b1d69ca4519934a8094e5
Priya Pandey <priyapa750@gmail.com>
2026-02-09 23:32:51 +0530

    do basic of js

---
+++ js/jsprograms/firstpracticeSet.js/firstPractice.js
@@ -0,0 +1,14 @@
+//write a js program to store name,phone numbers and marks by using object
+let details = {
+  name: "Priya Pandey",
+  "phone number": 9891202796,
+  marks: 340,
+};
+console.log(details);
+
+//create a variable of type string and try to add a number to it
+var str = "priya";
+console.log(str + 1);
+
+//use type of operator to find the datatype of the string in last question
+console.log(typeof str);
