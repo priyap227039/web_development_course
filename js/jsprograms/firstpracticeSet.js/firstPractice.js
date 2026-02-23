1ff6a12bbe89912cf98a69c7faf57078af9b82aa
Priya Pandey <priyapa750@gmail.com>
2026-02-23 23:50:24 +0530

    Update firstPractice.js

--- js/jsprograms/firstpracticeSet.js/firstPractice.js
+++ js/jsprograms/firstpracticeSet.js/firstPractice.js
@@ -12,3 +12,25 @@ console.log(str + 1);
 
 //use type of operator to find the datatype of the string in last question
 console.log(typeof str);
+
+//create a const oject in js can you change it to hold a number later
+const num = {
+  name: "priya",
+};
+console.log(num);
+// num = 10; // no it doesnt work bcoz we do not re intialize const variable
+
+//try to add a new key to the const object in problem 3 were you able to do it
+num.class = "fifth";
+console.log(num);
+
+//write a js program to create a word-meaning dictionary of 5 words
+const dictionary = {
+  courage: "The ability to do something that frightens you",
+  empathy: "The ability to understand and share feelings of others",
+  integrity: "The quality of being honest and having strong moral principles",
+  innovation: "Introducing new ideas or methods",
+  perseverance: "Continued effort despite difficulties",
+};
+console.log("complete dictionary", dictionary);
+
