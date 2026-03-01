a1ad4d2618cebbc297b2d040e864e34800ca0f80
Priya Pandey <priyapa750@gmail.com>
2026-03-02 00:16:06 +0530

    Add files via upload

---
+++ js/jsprograms/firstpracticeSet.js/secondpractice.js
@@ -0,0 +1,8 @@
+//use logicak opertors to find whether the age of a person lies between 10 and 20 ?
+let age=15;
+if(age>=10 && age<=20){
+    console.log("your age is lie between 10 and 20");
+}
+else{
+    console.log("your age is  not lie between 10 and 20");
+}
